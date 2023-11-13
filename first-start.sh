e9c2ec703d7287ab94686f0e29f312863b152e63
rafammpp <rafammpp@gmail.com>
2023-11-13 12:57:34 +0100

    setup aws from env

--- first-start.sh
+++ first-start.sh
@@ -1,14 +1,47 @@
 docker compose up -d;
-echo "Setup AWS CLI following this pattern:";
-echo "
-AWS Access Key ID [None]: <access_key_id>
-AWS Secret Access Key [None]: <access_key_secret>
-Default region name [None]: auto
-Default output format [None]: json
------------------------------------------------
-Generate access_key_id and access_key_secret following this guide https://developers.cloudflare.com/r2/api/s3/tokens/
------------------------------------------------";
-docker compose exec crontab bash -c "aws configure";
+if [ ! -f 'plausible-conf.env' ]; then
+    echo "-----------------------------------------------";
+    echo "ERROR: plausible-conf.env file not found. Please create it and try again.";
+    echo "-----------------------------------------------";
+    exit 1;
+fi
+source plausible-conf.env;
+
+# setup aws
+docker compose exec crontab bash -c "aws configure set aws_access_key_id ${R2_ACCESS_KEY_ID}";
+docker compose exec crontab bash -c "aws configure set aws_secret_access_key ${R2_SECRET_ACCESS_KEY}";
+docker compose exec crontab bash -c "aws configure set aws_default_region auto";
+docker compose exec crontab bash -c "aws configure set output json";
+
+# test aws cli
+echo "-----------------------------------------------";
+echo "Listing bucket for testing aws configuration...";
+docker compose exec crontab bash -c "aws s3 ls s3://$R2_BUCKET/ --endpoint-url $R2_ENDPOINT";
+
+# try to upload a test file to the bucket and check if it's there. If not, exit.
+echo "-----------------------------------------------";
+echo "Testing backup upload...";
+docker compose exec crontab bash -c "echo 'test' > /backup/test.txt";
+docker compose exec crontab bash -c "aws s3 cp /backup/test.txt s3://$R2_BUCKET/ --endpoint-url $R2_ENDPOINT";
+docker compose exec crontab bash -c "rm /backup/test.txt";
+docker compose exec crontab bash -c "aws s3 cp s3://$R2_BUCKET/test.txt /backup/test.txt --endpoint-url $R2_ENDPOINT";
+# check if the file is there
+if [ ! -f 'backup/test.txt' ]; then
+    echo "-----------------------------------------------";
+    echo "ERROR: Read/Write test failed. Set valid values for R2_BUCKET, R2_ACCESS_KEY_ID, R2_SECRET_ACCESS_KEY R2_ENDPOINT vars in plausible-conf.env and try again.";
+    echo "You can generate access_key_id and access_key_secret following this guide https://developers.cloudflare.com/r2/api/s3/tokens/";
+    echo "Ensure the bucket exists and the credentials have read/write access to it.";
+    echo "-----------------------------------------------";
+    rm backup/test.txt;
+    docker compose exec crontab bash -c "aws s3 rm s3://$R2_BUCKET/test.txt --endpoint-url $R2_ENDPOINT";
+    exit 1;
+fi
+rm backup/test.txt;
+docker compose exec crontab bash -c "aws s3 rm s3://$R2_BUCKET/test.txt --endpoint-url $R2_ENDPOINT";
+
+echo "All good!";
+echo "-----------------------------------------------";
+
 
 echo "type any domains you want to add to the plausible server, separated by a space";
 read -a domains;
@@ -28,7 +61,7 @@ for domain in "${domains[@]}"; do
         listen [::]:80;
 
         location / {
-            proxy_pass http://127.0.0.1:8000;
+            proxy_pass http://plausible:8000;
             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         }
 
