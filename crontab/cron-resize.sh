9ee241c48c55178306163062d1b14f5cef97beec
rafammpp <rafammpp@gmail.com>
2024-02-05 12:43:29 +0100

    script to detect activity on server

--- crontab/cron-resize.sh
+++ crontab/cron-resize.sh
@@ -1,11 +1,17 @@
+#!/bin/bash
+last_query=$(tail -n 100 logs/nginx/access.log | grep /api/stats/ | grep = | grep 200 | tail -n 1 |  grep -o -E "\[.+\]" | sed 's/[][]//g' | sed 's/\//-/g' | sed 's/:/ /' | sed 's/ +0000//');
 
-last_query=$(tail -n 100 logs/nginx/access.log | grep /api/stats/ | grep = | grep 200 | tail -n 1 |  grep --only-matching -E "\[.+\]" | sed 's/[][]//g' | sed 's/\//-/g' | sed 's/:/ /' | sed 's/ +0000//');
 
-# calculate the number of hours since the last query
-time_diff=$(date -d "$last_query" +%s) $(date +%s) | awk '{print ($2 - $1) / 3600}'
+# check if the last query is empty
+if [ -z "$last_query" ]; then
+    time_diff=3601;
+else
+    # calculate the number of seconds since the last query
+    time_diff=$( echo $(date -d "$last_query" +%s) $(date +%s) | awk '{print ($2 - $1)}');
+fi
 
 # check if the difference is greater than 1 hour
-if [ $time_diff -gt 1 ]; then
+if [ $time_diff -gt 3600 ]; then
     echo "The last query was more than 1 hour ago"
 else
     echo "The last query was less than 1 hour ago"
