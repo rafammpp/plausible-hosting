e25f116a80ef9cb9159ce5055e76ec8cd5135b0d
rafammpp <rafammpp@gmail.com>
2025-10-07 11:50:02 +0200

    remove certbot

--- crontab/scripts/cron-certbot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source /run/secrets/plausible-conf;
-if [ "$DISABLE_CRON_SCRIPTS" = true ] || [ -f /locks/setting-up.lock ]; then
-    exit 0;
-fi
-
-/usr/bin/certbot renew --quiet;
--- crontab/scripts/on-startup.sh
+++ crontab/scripts/on-startup.sh
@@ -1,3 +1,2 @@
-/scripts/cron-certbot.sh >> /logs/certbot-renew.log 2>&1;
 /scripts/cron-restore.sh >> /logs/restore-script.log 2>&1;
 /scripts/cron-logs-backup.sh >> /logs/logs-backup-script.log 2>&1;
