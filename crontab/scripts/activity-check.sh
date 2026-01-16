5bd7a19cc62c23ac23ddfcfcd6726f33c991d4cf
rafammpp <rafammpp@gmail.com>
2026-01-16 09:51:42 +0100

    fix: include /api/v2/query in last query check for activity log

--- crontab/scripts/activity-check.sh
+++ crontab/scripts/activity-check.sh
@@ -5,8 +5,9 @@ if [ -f /locks/archive.lock ]; then
     echo true;
     exit 0;
 fi
-
-last_query=$(cat /logs/plausible.log | grep -v /current-visitors | grep /api/stats/ | grep = | grep 200 | tail -n 1 | grep -oE '^[^ ]* ');
+# add /api/v2/query to take into account new plausible api
+# get the last query time from plausible log excluding /current-visitors
+last_query=$(cat /logs/plausible.log | grep -v /current-visitors | grep -E "/api/stats/|/api/v2/query" | grep = | grep 200 | tail -n 1 | grep -oE '^[^ ]* ');
 
 
 # check if the last query is empty
